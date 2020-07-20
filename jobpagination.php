b96abab9380ec9d82754b250be4f0a97f95378dd
MehakSachdeva20 <sachdevamehak38@gmail.com>
2020-07-20 18:16:34 +0530

    Add files via upload

---
+++ jobpagination.php
@@ -0,0 +1,36 @@
+<?php
+
+session_start();
+
+require_once("db.php");
+
+$limit = 4;
+
+if(isset($_GET["page"])) {
+	$page = $_GET['page'];
+} else {
+	$page = 1;
+}
+
+$start_from = ($page-1) * $limit;
+
+$sql = "SELECT * FROM users LIMIT $start_from, $limit";
+$result = $conn->query($sql);
+if($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		$sql1 = "SELECT * FROM users WHERE id_user='$row[id_user]'";
+              $result1 = $conn->query($sql1);
+              if($result1->num_rows > 0) {
+                while($row1 = $result1->fetch_assoc()) 
+                {
+             ?>
+
+<br><br><br><br><br><br>
+
+		<?php
+			}
+		}
+	}
+}
+
+$conn->close();
